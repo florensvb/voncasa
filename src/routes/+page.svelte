3d28348a7319f4803cba40784864d9cc36dcd30e
Florens von Buchwaldt <mail@florensvb.com>
2024-03-22 14:55:45 -0400

    Init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>VONCASA</h1>
+
+<style>
+	body {
+		background-color: bisque;
+	}
+	h1 {
+		color: #2b2f38;
+		text-align: center;
+	}
+</style>
